83bea30b09c3733bd335d885799010d117930c9e
LABIBSIDDIKY <labibsiddikyssjgod@gmail.com>
2026-09-17 19:05:56 +0600

    Initial commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,18 +4,22 @@ const path = require('path');
 
 const app = express();
 
-// Set views path relative to function location
 app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'ejs');
 
-// Serve static assets
 app.use(express.static(path.join(__dirname, '../../public')));
 
-// Routes
+// Core & Existing Routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/project-concepts', (req, res) => res.render('project-concepts'));
 app.get('/features', (req, res) => res.render('features'));
 app.get('/contributors', (req, res) => res.render('contributors'));
 app.get('/contact', (req, res) => res.render('contact'));
+
+// New Chapter Routes
+app.get('/chapter-1', (req, res) => res.render('chapter-1'));
+app.get('/chapter-2', (req, res) => res.render('chapter-2'));
+app.get('/chapter-3', (req, res) => res.render('chapter-3'));
+app.get('/chapter-4', (req, res) => res.render('chapter-4'));
 
 module.exports.handler = serverless(app);
--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// Core & Existing Routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/project-concepts', (req, res) => res.render('project-concepts'));
 app.get('/features', (req, res) => res.render('features'));
 app.get('/contributors', (req, res) => res.render('contributors'));
 app.get('/contact', (req, res) => res.render('contact'));
+
+// New Chapter Routes
+app.get('/chapter-1', (req, res) => res.render('chapter-1'));
+app.get('/chapter-2', (req, res) => res.render('chapter-2'));
+app.get('/chapter-3', (req, res) => res.render('chapter-3'));
+app.get('/chapter-4', (req, res) => res.render('chapter-4'));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
